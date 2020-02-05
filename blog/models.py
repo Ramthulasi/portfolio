0f1e93d7c398b4b224b013f31935834590026427
Thulasiram Toorpu <ramthulsi@ymail.com>
2020-02-05 19:06:22 +0530

    added jobs to home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    Title = models.CharField(max_length=200)
+    Pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')    
+
+
